e5312a0b0c39f03e0493c5bd8f2808f8b28ba7bb
gleb-gutnik <116025389+gleb-gutnik@users.noreply.github.com>
2022-10-18 15:31:41 +0300

    update data files

--- converter.py
+++ converter.py
@@ -2,126 +2,45 @@ from asyncore import write
 import os
 import json
 import collections
+from tqdm import tqdm
 unprocessed_docs = []
 files = os.listdir('json_data')
-for filename in files:
+filename = files[20]
+for filename in tqdm(files):
     try:
-        print(filename)
-        prepacked_entities = []
-        mentions = []
         with open('json_data' + '\\' + filename, encoding='utf-8') as json_file:
-            data = json.load(json_file)
-            for index, entity in enumerate(data['entities']):
-                for mention in entity:
-                    mentions.append(mention)
-                    start = int(mention[0])
-                    end = int(mention[1])
-                    prepacked_entities.append('{M' + str(index) + ' ' + data['text'][start:end] + ' }')
-        tuple_mentions = [tuple(i) for i in mentions]
-        iterator = zip(tuple_mentions, prepacked_entities)
-        mentions_dict = dict(iterator)
-        ordered_mentions_dict = collections.OrderedDict(sorted(mentions_dict.items()))
-        ordered_mentions = list(ordered_mentions_dict.values())
-
-        mentions.sort()
-
-        first_order_mentions = []
-        second_order_mentions = []
-        third_order_mentions = []
-        frth_order_mentions = []
-        ffth_order_mentions = []
-        sixth_order_mentions = []
-        seventh_order_mentions = []
-        eighth_order_mentions = []
-        ninth_order_mentions = []
-        tenth_order_mentions = []
-        for idx, mention in enumerate(mentions):
-            overlaps = []
-            current_mention = mentions[idx]
-            for tested_range in mentions:
-                if current_mention[0] >= tested_range[0] and tested_range[1] >= current_mention[1]:
-                    if not (current_mention[0]==tested_range[0] and current_mention[1]==tested_range[1]):
-                        overlaps.append(tested_range)
-            if len(overlaps) > 0:
-                if len(overlaps) == 1:
-                    second_order_mentions.append(current_mention)
-                elif len(overlaps) == 2:
-                    third_order_mentions.append(current_mention)
-                elif len(overlaps) == 3:
-                    frth_order_mentions.append(current_mention)
-                elif len(overlaps) == 4:
-                    ffth_order_mentions.append(current_mention)
-                elif len(overlaps) == 5:
-                    sixth_order_mentions.append(current_mention)
-            else:
-                first_order_mentions.append(current_mention)
-
-        for som in second_order_mentions:
-            som_string = ordered_mentions_dict[tuple(som)]
-            som_raw = data['text'][som[0]:som[1]]
-            
-            for fom in first_order_mentions:
-                if som[0] >= fom[0] and fom[1] >= som[1]:
-                    fom_string = ordered_mentions_dict[tuple(fom)]
-                    
-                    fom_raw = data['text'][fom[0]:fom[1]]
-                    adjustment = len(fom_string) - len(fom_raw) - 2
-                    if not (som[0] == fom[0] and som[1] == fom[1]):
-                        starter = som[0] - fom[0] + adjustment
-                        ordered_mentions_dict[tuple(fom)] = ordered_mentions_dict[tuple(fom)][0:starter] + som_string + fom_string[starter+len(som_raw):]
-
-        for som in third_order_mentions:
-            som_string = ordered_mentions_dict[tuple(som)]
-            som_raw = data['text'][som[0]:som[1]]
-            
-            for fom in first_order_mentions:
-                if som[0] >= fom[0] and fom[1] >= som[1]:
-                    fom_string = ordered_mentions_dict[tuple(fom)]
-                    
-                    fom_raw = data['text'][fom[0]:fom[1]]
-                    adjustment = len(fom_string) - len(fom_raw) - 2
-                    if not (som[0] == fom[0] and som[1] == fom[1]):
-                        starter = som[0] - fom[0] + adjustment
-                        ordered_mentions_dict[tuple(fom)] = ordered_mentions_dict[tuple(fom)][0:starter] + som_string + fom_string[starter+len(som_raw):]
-
-        for som in frth_order_mentions:
-            som_string = ordered_mentions_dict[tuple(som)]
-            som_raw = data['text'][som[0]:som[1]]
-            
-            for fom in first_order_mentions:
-                if som[0] >= fom[0] and fom[1] >= som[1]:
-                    fom_string = ordered_mentions_dict[tuple(fom)]
-                    
-                    fom_raw = data['text'][fom[0]:fom[1]]
-                    adjustment = len(fom_string) - len(fom_raw) - 2
-                    if not (som[0] == fom[0] and som[1] == fom[1]):
-                        starter = som[0] - fom[0] + adjustment
-                        ordered_mentions_dict[tuple(fom)] = ordered_mentions_dict[tuple(fom)][0:starter] + som_string + fom_string[starter+len(som_raw):]
-        
-        for som in ffth_order_mentions:
-            som_string = ordered_mentions_dict[tuple(som)]
-            som_raw = data['text'][som[0]:som[1]]
-            
-            for fom in first_order_mentions:
-                if som[0] >= fom[0] and fom[1] >= som[1]:
-                    fom_string = ordered_mentions_dict[tuple(fom)]
-                    
-                    fom_raw = data['text'][fom[0]:fom[1]]
-                    adjustment = len(fom_string) - len(fom_raw) - 2
-                    if not (som[0] == fom[0] and som[1] == fom[1]):
-                        starter = som[0] - fom[0] + adjustment
-                        ordered_mentions_dict[tuple(fom)] = ordered_mentions_dict[tuple(fom)][0:starter] + som_string + fom_string[starter+len(som_raw):]
-        result = ''
-        prev = 0
-        for fom in first_order_mentions:
-            result += data['text'][prev:fom[0]]
-            prev = fom[1]
-            result += ordered_mentions_dict[tuple(fom)]
-        result += data['text'][prev:]
-        with open('sacr_data' + '\\' + filename + '.sacr', 'w', encoding='utf-8') as output:
-            output.write(result)
-            output.close()
+                data = json.load(json_file)
+                json_file.close()
+        entities = data["entities"]
+        text = data["text"]
+        mentions = []
+        entity_count = 0
+        entity_dict = {}
+        for entity in entities:
+            entity_count += 1
+            for mention in entity:
+                mentions.append(mention)
+                entity_dict[tuple(mention)] = entity_count
+        gone_through_left = []
+        gone_through_right = []
+        for w in sorted(mentions, key=lambda x:-x[0]):
+            # if there were right-hand brackets inserted whose offset is less than the current left bracket, we add 2 as per such bracket
+            alignment = sum(w[0] >= i for i in gone_through_right) * 2
+            # if there were left-hand brackets inserted whose offset is less than the current left bracket, we add 6 as per such bracket (3 for '{M ', and 3 for entity id)
+            alignment += sum(w[0] >= i for i in gone_through_left) * 6
+            # accordingly for current right bracket...
+            alignment = sum(w[1] >= i for i in gone_through_right) * 2
+            alignment += sum(w[1] >= i for i in gone_through_left) * 6            
+            entity_num = '{M' + str(entity_dict[tuple(w)]).zfill(3) + ' '
+            text = text[:w[0]] + entity_num + text[w[0]:alignment+w[1]] + ' }' + text[alignment+w[1]:]
+            gone_through_left.append(w[0])
+            gone_through_right.append(w[1])
+        output_filename = filename.replace('.json', '.sacr')
+        with open('sacr_data' + '\\' + output_filename, 'w', encoding='utf-8') as output_file:
+            output_file.write(text)
+            output_file.close()
     except:
+        print("Parsing error.")
         unprocessed_docs.append(filename)
         continue
 print('Conversion completed.')
--- loop_convert.py
+++ loop_convert.py
@@ -1,8 +1,8 @@
 import os
+from tqdm import tqdm
 path = 'sacr_data'
 filenames = os.listdir(path)
-for filename in filenames:
-    print(filename)
+for filename in tqdm(filenames):
     outfile = filename.replace('.json', '')
     outfile = outfile.replace('.sacr', '')
     os.system('python sacr2conll.py -o conll_data/{}.conll sacr_data/{}'.format(outfile, filename))
